9a65889c99a7127dfb4ad0036eaf64ff21b0a34e
yuliyaBir <zhivo4ka@gmail.com>
2023-08-23 08:02:14 +0200

    Initial commit

---
+++ src/main/java/be/vdab/keuken/KeukenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.keuken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeukenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeukenApplication.class, args);
+    }
+
+}
